a739dad88a583f8d467564a92b68a15bec0223a8
rhilenova <rhilenova@gmail.com>
2017-01-19 19:57:33 -0500

    Working on GUI. Top level buttons added. Need to re-size some things.

--- src/main/java/space/rhilenova/mc/admin_chat_tools/GuiConfigure.java
+++ src/main/java/space/rhilenova/mc/admin_chat_tools/GuiConfigure.java
@@ -2,13 +2,28 @@ package space.rhilenova.mc.admin_chat_tools;
 
 import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.gui.GuiTextField;
 
 import java.io.IOException;
 
 public class GuiConfigure extends GuiScreen
 {
+    private GuiTextField group_name_field;
+    private GuiTextField pattern_field;
+    private GuiTextField fore_field;
+    private GuiTextField back_field;
+
     public void initGui()
     {
+        int x = 0;
+        this.buttonList.clear();
+        this.group_name_field = new GuiTextField(x++, this.fontRendererObj, 115, 28, 172, 20);
+        this.buttonList.add(new GuiButton(x++, 291, 28, 20, 20, "+"));
+        this.pattern_field = new GuiTextField(x++, this.fontRendererObj, 15, 52, 172, 20);
+        this.buttonList.add(new GuiButton(x++, 191, 52, 20, 20, "+"));
+        this.fore_field = new GuiTextField(x++, this.fontRendererObj, 216, 52, 96, 20);
+        this.back_field = new GuiTextField(x++, this.fontRendererObj, 316, 52, 96, 20);
+        this.buttonList.add(new GuiButton(x++, 111, 205, 200, 20, "Done"));
     }
 
     protected void actionPerformed(GuiButton button) throws IOException
@@ -21,6 +36,16 @@ public class GuiConfigure extends GuiScreen
 
     public void drawScreen(int mouseX, int mouseY, float partialTicks)
     {
-        this.drawCenteredString(this.fontRendererObj, "I'm working!", this.width / 2, 40, 16777215);
+        this.drawDefaultBackground();
+        this.drawCenteredString(this.fontRendererObj, "Notification Settings", this.width / 2, 15, 16777215);
+        this.drawString(this.fontRendererObj, "Groups", 15, 76, 16777215);
+        this.drawString(this.fontRendererObj, "Tokens", 216, 76, 16777215);
+        this.group_name_field.drawTextBox();
+        this.pattern_field.drawTextBox();
+        this.fore_field.drawTextBox();
+        this.back_field.drawTextBox();
+        this.drawGradientRect(15, 85, 210, 200, 0xFF000000, 0xFF000000);
+        this.drawGradientRect(216, 85, 411, 200, 0xFF000000, 0xFF000000);
+        super.drawScreen(mouseX, mouseY, partialTicks);
     }
 }
